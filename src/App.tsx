ddce049d968b10fc017ee249a96eea5a483b9d19
Jan Kozera <jankozera@Jans-MacBook-Pro.local>
2023-06-16 16:23:03 +0200

    created react app with required libraries

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* Theme variables */
+import './theme/index.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="font-semibold text-4xl">React TS starter</h1>
+    </div>
+  );
+};
+
+export default App;
