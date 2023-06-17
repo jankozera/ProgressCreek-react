07d3d55773a259a0efb1529f84e6e90ab43a668e
Jan Kozera <jankozera@Jans-MacBook-Pro.local>
2023-06-17 16:56:04 +0200

    fixed login page styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,11 +6,11 @@ import UIHeading from '../components/UICommon/UIHeading';
 
 const LoginPage: FC = () => {
     return (
-        <div className='w-full flex space-x-12'>
-            <div className="w-2/3 flex justify-end">
+        <div className='w-full flex space-x-12 pt-8'>
+            <div className="w-2/3 flex justify-end pr-16">
                 <img src={img} alt="" />
             </div>
-            <div className="w-1/3">
+            <div className="w-1/3 pt-16">
                 <UIHeading title='Log in' />
                 <p className='font-normal text-black-primary mb-8'>
                     Complete the fields below to access your account.
